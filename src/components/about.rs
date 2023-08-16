07f73bc2e87142240598125b47b2356c25f86aa5
David Wallace Croft <david@CroftSoft.com>
2023-08-16 07:22:24 -0500

    about page

--- src/components/about.rs
+++ src/components/about.rs
@@ -8,11 +8,34 @@ pub fn About(cx: Scope) -> Element {
     h1 {
       "About"
     }
+    p {
+      "The Dallas Rust Users Meetup (DRUM) is a ",
+      a {
+        href: "https://www.rust-lang.org/",
+        target: "_blank",
+        "Rust programming language",
+      }
+      " users group. ",
+    }
+    p {
+      "Join our ",
+      a {
+        href: "https://www.meetup.com/dallasrust/",
+        target: "_blank",
+        "Meetup group",
+      },
+      " for an invitations to join our Discord meetings ",
+      "and to access our bulletin boards."
+    }
     h2 {
       "Contact"
     }
     p {
-      "David Wallace Croft <david@CroftSoft.com>"
+      "David Wallace Croft ",
+      a {
+        href: "mailto:david@croftsoft.com",
+        "<david@CroftSoft.com>"
+      }
     }
     }
   }
