890ca7f3095d92c4066747081306c8b4ef803b48
David Wallace Croft <david@CroftSoft.com>
2023-08-19 14:47:31 -0500

    removed layout

--- src/route.rs
+++ src/route.rs
@@ -2,20 +2,17 @@ use crate::components::about::About;
 use crate::components::colophon::Colophon;
 use crate::components::home::Home;
 use crate::components::page_not_found::PageNotFound;
-use crate::components::page_template::PageTemplate;
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 
 #[derive(Clone, Routable)]
 pub enum Route {
-  #[layout(PageTemplate)]
   #[route("/")]
   Home {},
   #[route("/about")]
   About {},
   #[route("/colophon")]
   Colophon {},
-  #[end_layout]
   #[route("/:..route")]
   PageNotFound {
     route: Vec<String>,
